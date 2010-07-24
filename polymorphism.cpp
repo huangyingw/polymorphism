a1ee13e39f975930b17283e19b62daf1cd0ab83a
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-24 15:22:50 +0800

    n

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -2,27 +2,31 @@
 #include <iostream>
 using namespace std;
 
-class CPolygon {
+class CPolygon 
+{
   protected:
     int width, height;
   public:
-    void set_values (int a, int b)
-      { width=a; height=b; }
-  };
+    void set_values(int a, int b)
+    	{ width=a; height=b; }
+};
 
-class CRectangle: public CPolygon {
+class CRectangle: public CPolygon 
+{
   public:
     int area ()
-      { return (width * height); }
-  };
+    	{ return (width * height); }
+};
 
-class CTriangle: public CPolygon {
+class CTriangle: public CPolygon 
+{
   public:
     int area ()
-      { return (width * height / 2); }
-  };
+    	{ return (width * height / 2); }
+};
 
-int main () {
+int main () 
+{
   CRectangle rect;
   CTriangle trgl;
   CPolygon * ppoly1 = &rect;
